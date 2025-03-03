136571f2776eec9f170cc688da5e06e597be329f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-03 12:17:08 -0500

    update Thanks model with sample data extensions

--- Thankful-List-iOS/Models/Thanks.swift
+++ Thankful-List-iOS/Models/Thanks.swift
@@ -9,7 +9,8 @@ import SwiftUI
 import SwiftData
 
 @Model
-class Thanks {
+class Thanks: Identifiable {
+    var id = UUID()
     var title: String
     var body: String
     var date: Date
@@ -25,3 +26,16 @@ class Thanks {
     }
 }
 
+extension Thanks {
+    @MainActor
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(for: Thanks.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        container.mainContext.insert(Thanks(title: "My home", body: "I am thankful for the roof over my head", date: Date(), isFavorite: false))
+        container.mainContext.insert(Thanks(title: "My car", body: "My car takes me where I need to go", date: Date(), isFavorite: true))
+        container.mainContext.insert(Thanks(title: "My freinds", body: "They help my out", date: Date(), isFavorite: true))
+        container.mainContext.insert(Thanks(title: "Food", body: "It nourishes my body", date: Date(), isFavorite: false))
+        container.mainContext.insert(Thanks(title: "TV", body: "It keeps me entertained", date: Date(), isFavorite: false))
+        container.mainContext.insert(Thanks(title: "Radio", body: "For when the TV doesn't work", date: Date(), isFavorite: true))
+    }
+}
