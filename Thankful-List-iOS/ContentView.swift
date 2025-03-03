25c4c42c46c5743818ef7f69ee4e04e22ebee37e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-03 12:30:42 -0500

    update ContentView with TabView

--- Thankful-List-iOS/ContentView.swift
+++ Thankful-List-iOS/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Add Thanks", systemImage: "heart.square") {
+                NavigationStack {
+                    AddThanksView()
+                }
+            }
         }
-        .padding()
     }
 }
 
