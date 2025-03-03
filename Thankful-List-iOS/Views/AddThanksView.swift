289500702e85214a83cc34d64293773b14145b97
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-03 12:30:20 -0500

    add AddThanksView to project

---
+++ Thankful-List-iOS/Views/AddThanksView.swift
@@ -0,0 +1,18 @@
+//
+//  AddThanksView.swift
+//  Thankful-List-iOS
+//
+//  Created by Thomas Cowern on 3/3/25.
+//
+
+import SwiftUI
+
+struct AddThanksView: View {
+    var body: some View {
+        Text("This is the AddThanksView")
+    }
+}
+
+#Preview {
+    AddThanksView()
+}
