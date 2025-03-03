a00d1e222d0e718127ebcc42c2316b9856f77537
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-03 08:58:55 -0500

    Thankful-List-iOS initial commit

---
+++ Thankful-List-iOS/Thankful_List_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thankful_List_iOSApp.swift
+//  Thankful-List-iOS
+//
+//  Created by Thomas Cowern on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Thankful_List_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
