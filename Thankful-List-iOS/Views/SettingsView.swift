1cf1dcad52de45087ab03d9e33589d1213570e48
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-04 10:12:17 -0500

    add SettingsView to Views folder

---
+++ Thankful-List-iOS/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Thankful-List-iOS
+//
+//  Created by Thomas Cowern on 3/4/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
