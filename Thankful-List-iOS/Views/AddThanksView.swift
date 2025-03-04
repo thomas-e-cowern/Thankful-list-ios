04af5005cfa1f349fe49a96820636e0ac32edd44
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-04 08:16:04 -0500

    update AddThanksView to fix text alignment issue

--- Thankful-List-iOS/Views/AddThanksView.swift
+++ Thankful-List-iOS/Views/AddThanksView.swift
@@ -17,16 +17,18 @@ struct AddThanksView: View {
     var body: some View {
         Form {
             TextField("What are you grateful for?", text: $title)
+            
             ZStack(alignment: .leading) {
-                if reason.isEmpty {
-                    Text("Write a reason why you're grateful.")
-                        .foregroundColor(.secondary.opacity(0.5))
+                VStack(alignment: .leading) {
+                    if reason.isEmpty {
+                        Text("Write a reason why you're grateful.")
+                            .foregroundColor(.secondary.opacity(0.5))
+                    }
                     
+                    TextEditor(text: $reason)
+                        .frame(height: 150)
+                        .foregroundColor(reason == reason ? .gray : .primary)
                 }
-                
-                TextEditor(text: $reason)
-                    .frame(height: 150)
-                    .foregroundColor(reason == reason ? .gray : .primary)
             }
             
             Toggle("Mark as Favorite", isOn: $isFavorite)
