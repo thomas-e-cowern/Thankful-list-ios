3ac3a64e6fa13b450ffced26e22948befedb9b3d
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-23 09:16:59 -0400

    add TLCustomColors to project

---
+++ Thankful-List-iOS/Utilities/TLCustomColors.swift
@@ -0,0 +1,17 @@
+//
+//  TLCustomColorsa.swift
+//  Thankful-List-iOS
+//
+//  Created by Thomas Cowern on 6/23/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct TLCustomColors {
+    static let accentsColors: Color = Color("AccentColors")
+    static let backgroundColors: Color = Color("BackgroundColors")
+    static let highLights: Color = Color("HighLights")
+    static let primaryColors: Color = Color("PrimaryColors")
+    static let textColors: Color = Color("TextColors")
+}
