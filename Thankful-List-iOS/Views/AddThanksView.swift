92823485b1ad88e5ff114b648f41f6efb0097194
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-25 08:39:56 -0400

    update AddThanksView with favorite button

--- Thankful-List-iOS/Views/AddThanksView.swift
+++ Thankful-List-iOS/Views/AddThanksView.swift
@@ -13,25 +13,35 @@ struct AddThanksView: View {
     @State private var reason: String = ""
     @State private var isFavorite: Bool = false
     
-    
     var body: some View {
-        Form {
-            TextField("What are you grateful for?", text: $title)
-            
-            ZStack(alignment: .leading) {
-                VStack(alignment: .leading) {
-                    if reason.isEmpty {
-                        Text("Write a reason why you're grateful.")
-                            .foregroundColor(.secondary.opacity(0.5))
+        VStack {
+            Form {
+                TextField("What are you grateful for?", text: $title)
+                
+                ZStack(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        if reason.isEmpty {
+                            Text("Write a reason why you're grateful.")
+                                .foregroundColor(.secondary.opacity(0.5))
+                        }
+                        
+                        TextEditor(text: $reason)
+                            .frame(height: 150)
+                            .foregroundColor(reason == reason ? .gray : .primary)
                     }
+                }
+                HStack {
+                    Text("Mark as favorite")
                     
-                    TextEditor(text: $reason)
-                        .frame(height: 150)
-                        .foregroundColor(reason == reason ? .gray : .primary)
+                    Spacer()
+                    
+                    Button {
+                        isFavorite.toggle()
+                    } label: {
+                        isFavorite ? Image(systemName: "heart.fill") : Image(systemName: "heart")
+                    }
                 }
             }
-            
-            Toggle("Mark as Favorite", isOn: $isFavorite)
         }
     }
 }
