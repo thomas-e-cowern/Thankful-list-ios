418481273a3b3674194da50561ab9b6401f53e3c
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-25 09:34:47 -0400

    update SettingsView, add Buttons and formatting

--- Thankful-List-iOS/Views/SettingsView.swift
+++ Thankful-List-iOS/Views/SettingsView.swift
@@ -9,9 +9,46 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        VStack {
-            Text("Settings View")
+        VStack(spacing: 30) {
+            
+            Section("Data") {
+                VStack(spacing: 20) {
+                        Text("The button below will erase all your data and start fresh.")
+                        
+                    VStack {
+                        Text("Caution:")
+                            .font(.title2)
+                        Text("This will erase all your data and is irreversible")
+                    }
+                    .foregroundStyle(.red).bold()
+                    
+                        
+                    HStack(spacing: 30) {
+                        Text("Erase all data?")
+                        Button(role: .destructive) {
+                            // More to come...
+                        } label: {
+                            Text("Erase data")
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+                
+                Divider()
+                    .foregroundStyle(.red)
+                
+                HStack(spacing: 30) {
+                    Text("Export all data?")
+                    Button() {
+                        // More to come...
+                    } label: {
+                        Text("Export data")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
+        .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background {
             Color(TLCustomColors.backgroundColors)
