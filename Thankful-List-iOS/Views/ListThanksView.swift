603fd060e3a9b241bf788b973035e02b89424503
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-26 08:57:03 -0400

    update ListThanksView to use ThanksRowView

--- Thankful-List-iOS/Views/ListThanksView.swift
+++ Thankful-List-iOS/Views/ListThanksView.swift
@@ -14,37 +14,31 @@ struct ListThanksView: View {
     @Query private var thanksList: [Thanks]
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(thanksList) { thanks in
-                    HStack {
-                        
-                        Image(systemName: thanks.icon)
-                            .foregroundStyle(thanks.hexColor.opacity(0.8))
-                            .padding(.trailing, 5)
-                        
-                        VStack(alignment: .leading) {
-                            Text(thanks.title)
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(thanksList) { thanks in
+                        HStack {
                             
-                            Text("\(thanks.date, style: .date)")
-                                .font(.caption)
+                            NavigationLink(value: thanks) {
+                                ThanksRowView(thanks: thanks)
+                            }
                         }
-                        .foregroundStyle(TLCustomColors.textColors)
-                        
-                        Spacer()
-                        Image(systemName: "heart.fill")
-                            .foregroundStyle(.red)
+                    }
+                }
+                .background(TLCustomColors.backgroundColors)
+                .scrollContentBackground(.hidden)
+                .overlay {
+                    if thanksList.isEmpty {
+                        ContentUnavailableView("You don't have any Thanks yet!  ", image: "heart.square", description: Text("Add something your thankful for to begin the list!"))
+                            .background(TLCustomColors.backgroundColors)
+                            .foregroundStyle(TLCustomColors.textColors)
                     }
                 }
             }
-            .background(TLCustomColors.backgroundColors)
-            .scrollContentBackground(.hidden)
-            .overlay {
-                if thanksList.isEmpty {
-                    ContentUnavailableView("You don't have any Thanks yet!  ", image: "heart.square", description: Text("Add something your thankful for to begin the list!"))
-                        .background(TLCustomColors.backgroundColors)
-                        .foregroundStyle(TLCustomColors.textColors)
-                }
+            .navigationTitle("Thankful List")
+            .navigationDestination(for: Thanks.self) { thanks in
+                Text(thanks.title)
             }
         }
         
