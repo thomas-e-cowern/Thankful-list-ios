88184cc1f34b43067f9f90386f02572e7b1fa37f
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-30 07:57:48 -0400

    add IconImages enum to project

--- Thankful-List-iOS/Utilities/IconImages.swift
+++ Thankful-List-iOS/Utilities/IconImages.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum Icons: String, CaseIterable, Identifiable {
+    case star = "star.fill"
+    case swirl = "swirl.circle.righthalf.filled.inverse"
+    case circle = "circle.dotted.circle.fill"
+    case person = "person.fill"
+    case sun = "sun.min.fill"
+    var id: Self { self }
+}
