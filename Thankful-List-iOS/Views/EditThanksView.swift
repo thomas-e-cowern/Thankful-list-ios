fda0a8520fdc45411d481b114605d469ec607623
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-02 08:10:07 -0400

    update EditThanksView to add icon and color picker

--- Thankful-List-iOS/Views/EditThanksView.swift
+++ Thankful-List-iOS/Views/EditThanksView.swift
@@ -12,16 +12,56 @@ struct EditThanksView: View {
     @Bindable var thanks: Thanks
     @Environment(\.dismiss) var dismiss
     
+    @State private var showIconView: Bool = false
+    @State private var selectedIcon: Icons = .star
+    @State private var selectedColor: Color = .blue
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 100))
+    ]
+    
     var body: some View {
-        Form {
+        VStack {
+            Form {
+                Section {
+                    TextField("I'm grateful for...", text: $thanks.title)
+                    TextField("Why?", text: $thanks.reason)
+                    HStack {
+                        Text("Mark as favorite")
+                        
+                        Spacer()
+                        
+                        Button {
+                            thanks.isFavorite.toggle()
+                        } label: {
+                            thanks.isFavorite ? Image(systemName: "heart.fill") : Image(systemName: "heart")
+                        }
+                    }
+                    Image(systemName: thanks.icon)
+                        .foregroundStyle(thanks.hexColor)
+                }
+                
+                
+            }
+            
             Section {
-                TextField("I'm grateful for...", text: $thanks.title)
-                TextField("Why?", text: $thanks.reason)
-                Button("Ok") {
-                    dismiss()
+                HStack() {
+                    Button("Save") {
+                        dismiss()
+                    }
+                    .buttonStyle(.bordered)
+                    
+                    Spacer()
+                    
+                    Button("Cancel") {
+                        // More to come...
+                        dismiss()
+                    }
+                    .buttonStyle(.bordered)
                 }
             }
         }
+        .navigationTitle("\(thanks.title)")
     }
 }
 
